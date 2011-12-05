fcbdde79411ce358d1446b84b5cde94410759c57
Jonathan Christian <jchristian>
2011-12-05 14:49:10 -0800

    Pushing new changes

--- source/prep/collections/Where.cs
+++ source/prep/collections/Where.cs
@@ -5,10 +5,24 @@ namespace prep.collections
 {
   public class Where<ItemToMatch>
   {
-    public static sfsdf has_a<PropertyType>(Func<ItemToMatch, PropertyType> accessor)
+    public static CriteriaFactory<ItemToMatch, PropertyType> has_a<PropertyType>(Func<ItemToMatch, PropertyType> accessor)
     {
-      throw new NotImplementedException();
+        return new CriteriaFactory<ItemToMatch, PropertyType>(accessor);
     }
   }
 
+    public class CriteriaFactory<ItemToMatch, PropertyType>
+    {
+        private readonly Func<ItemToMatch, PropertyType> _accessor;
+
+        public CriteriaFactory(Func<ItemToMatch, PropertyType> accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public IMatchAn<ItemToMatch> equal_to(PropertyType value)
+        {
+            return new AnonymousMatch<ItemToMatch>(movie => _accessor(movie).Equals(value));
+        }
+    }
 }
